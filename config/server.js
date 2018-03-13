4b9f971ec4af1a1ef953a05456bbf2af939ddbd9
Rodrigo Henriques <rodrigo.henriques@99taxis.com>
2018-03-13 11:49:19 -0300

    Remove middleware

--- config/server.js
+++ config/server.js
@@ -8,11 +8,6 @@ const server = express()
 middleware.bind(server)
 router.bind(server)
 
-server.use('*', function (req, res) {
-    res.status(404)
-    res.send()
-})
-
 db.connect()
 
 server.listen(8080, () => console.log('Example app listening on port 8080!'))
